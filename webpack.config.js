d7fc2831e585e4d70b9cae2e01a28dd472a2c6b9
joggi83 <johan.decuyper@gmail.com>
2025-01-15 17:30:14 +0400

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'OrangeHelpSearch',
+    libraryTarget: 'umd',
+    globalObject: 'this'
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  mode: 'production'
+};
